fecc288e7899cfb1886878a481ea33d5d9ef5f23
jdetok <jdeko17@gmail.com>
2026-01-19 22:47:03 -0600

    spinning cats

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [changeBy, setChangeBy] = useState(1);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://jdeko.me" target="_blank">
+          <img src="https://jdeko.me/img/pets/rosie.png" className="logo" alt="jdme" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://jdeko.me" target="_blank">
+          <img src="https://jdeko.me/img/pets/guinness.png" className="logo" alt="jdme" />
+        </a>
+        <a href="https://jdeko.me" target="_blank">
+          <img src="https://jdeko.me/img/pets/milk.png" className="logo" alt="jdme" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>react practice</h1>
+      <div>increase by:</div>
+      <input type="number" value={changeBy} onChange={(e) => setChangeBy(Number(e.target.value))} />
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setCount((count) => count + changeBy)}>
+          count + {changeBy}
+        </button>
+        <button onClick={() => setCount((count) => count - changeBy)}>
+          count - {changeBy}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          count is {count}
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+export default App;
